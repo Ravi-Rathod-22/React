a7a6a7b9da7d27a145989d7af6d8d732f5d5850e
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2024-08-28 19:48:17 +0530

    Chapter 01 - Inception

---
+++ Chapter 01 - Inception/App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement('h1', {id:'intro'}, "Hello, My Name Is Ravi Rathod");
+
+const heading2 = React.createElement('h1', {id:'description'}, "I am here to learn React");
+
+const container = React.createElement("div", {id:'container'}, [heading , heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(container)
