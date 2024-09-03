02db69b56e918f28fd45231fb176222b28656ec1
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2024-09-03 19:57:55 +0530

    2. Igniting our App

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement('h1', {id:'intro'}, "Hello, My Name Is Ravi Rathod");
 
 const heading2 = React.createElement('h1', {id:'description'}, "I am here to learn React");
